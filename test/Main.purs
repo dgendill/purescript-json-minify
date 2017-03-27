69cc0f8e2c6fda29d6e8ead8246ef2d5a96550e1
Dominick Gendill <dgendill@gmail.com>
2017-03-27 15:33:07 -0600

    type annotation

--- test/Main.purs
+++ test/Main.purs
@@ -2,5 +2,11 @@ module Test.Main where
 
 import Test.JSON.Minify as T
 
+import Prelude
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Random (RANDOM)
+
 main :: forall e. Eff ( console :: CONSOLE, random :: RANDOM, exception :: EXCEPTION | e) Unit
 main = T.main
